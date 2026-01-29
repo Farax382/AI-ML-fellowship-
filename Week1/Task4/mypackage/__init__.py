74f07218de0b81bd9bf0b8aea064b5556aecb065
Farax382 <malikfarax382@gmail.com>
2026-01-29 18:19:51 +0500

    Add files via upload

---
+++ Week1/Task4/mypackage/__init__.py
@@ -0,0 +1,3 @@
+from .math_ops import *
+from .string_ops import *
+from .utils import *
---
+++ Week1/Task4/mypackage/string_ops.py
@@ -0,0 +1,6 @@
+def reverse_string(text):
+    return text[::-1]
+
+
+def capitalize_text(text):
+    return text.upper()
